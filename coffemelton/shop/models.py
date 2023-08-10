045fdba5d1047dc91a3994394f99d45163c8600a
kkreutzberg <kristikr@gmail.com>
2023-08-11 00:02:09 +0300

    Add Product model

---
+++ coffemelton/shop/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+
+CATEGORY_CHOICES = (
+    ('K', 'Kohv'),
+    ('T', 'Tee'),
+    ('V', 'Vein'),
+    ('G', 'Gurmee'),
+)
+
+SUBCATEGORY_CHOICES = (
+    ('oa', 'Oakohv'),
+    ('jahv', 'Jahvatatud kohv'),
+)
+
+
+# class Category(models.Model):
+#     name = models.CharField(max_length=30)
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Subcategory(models.Model):
+#     name = models.CharField(max_length=30)
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=1, default='K')
+    subcategory = models.CharField(choices=SUBCATEGORY_CHOICES, max_length=4, default='A')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    slug = models.SlugField(max_length=50, unique=True)
+    weight = models.IntegerField()
+    # image = models.ImageField()
+
+    def __str__(self):
+        return self.name
