f0f4060fcc177f8696e19c53aa096f135b7ff514
kkreutzberg <kristikr@gmail.com>
2023-08-11 13:06:23 +0300

    Add templates, updated models and views

--- coffemelton/shop/views.py
+++ coffemelton/shop/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "product_list.html"
+    context_object_name = 'products'
+    paginate_by = 12
+
+    # def get_queryset(self):
+    #     # You can filter products based on categories and subcategories here
+    #     return Product.objects.all()
