386ac9b732f0c75f6abe5bb9d26faa41dbf24493
LiisLi <liis.liias@gmail.com>
2023-08-14 21:46:06 +0300

    New product_detail model, view, template

--- coffemelton/shop/models.py
+++ coffemelton/shop/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=30, default='tootekategooria', unique=True)
+    category_name = models.CharField(max_length=30, default='tootekategooria', unique=True, blank=False, null=False)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.category_name
@@ -14,7 +17,10 @@ class Category(models.Model):
 
 class Subcategory(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    subcategory_name = models.CharField(max_length=30, default='alamkategooria', unique=True)
+    subcategory_name = models.CharField(max_length=30, default='alamkategooria', unique=True, blank=False, null=False)
+
+    class Meta:
+        verbose_name_plural = 'Subcategories'
 
     def __str__(self):
         return self.subcategory_name
@@ -23,12 +29,15 @@ class Subcategory(models.Model):
 # which are mandatory fields (null=...); hinnale vaja juurde saada euromärk ette?
 class Product(models.Model):
     subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    product_name = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
     slug = models.SlugField(max_length=50, unique=True)
-    weight = models.CharField(max_length=6)
-    image = models.ImageField(upload_to='images/', default='default.png')
+    weight = models.CharField(max_length=10, blank=True, null=True)
+    image = models.ImageField(upload_to='images/', default='default.png', blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Products'
 
     def __str__(self):
         return self.product_name
